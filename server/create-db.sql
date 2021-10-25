b19d639f5f9a0a576daf9b03b78e0878a5083316
jackson22burns <burns2je@dukes.jmu.edu>
2021-10-25 17:25:05 -0400

    breh

--- server/create-db.sql
+++ server/create-db.sql
@@ -3,7 +3,7 @@ DROP DATABASE IF EXISTS recomendations;
 DROP USER IF EXISTS rec_user@localhost;
 
 CREATE DATABASE recomendations CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-CREATE USER rec_user@localhost IDENTIFIED WITH mysql_native_password BY 'j_BREEZY22';
+CREATE USER rec_user@localhost IDENTIFIED WITH mysql_native_password BY 'sunsh1n3';
 GRANT ALL PRIVILEGES ON recomendations.* TO rec_user@localhost;
 
 
