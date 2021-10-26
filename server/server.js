1cdda7ac811f37bc1e60aea015b6916c55e31d21
jackson22burns <burns2je@dukes.jmu.edu>
2021-10-26 19:58:59 -0400

    npm

--- server/server.js
+++ server/server.js
@@ -5,3 +5,11 @@ const port = 5000;
 service.listen(port, () => {
   console.log(`We're live on port ${port}!`);
 });
+
+/*  
+    The HTTP specification defines several different kinds of requests—or methods—that 
+    clients can send to services. These methods include GET, POST, DELETE, and PATCH. 
+    These methods roughly correspond to the four basic operations you use to interact 
+    with records in a database: create, read, update, or delete. This collection of 
+    operations is sometimes abbreviated as CRUD.
+*/
