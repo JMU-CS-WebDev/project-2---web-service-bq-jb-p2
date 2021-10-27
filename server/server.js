57017ad81d9f769434609ace8e2910db7eb03c53
Brandon P. Quinn <quinnbp@dukes.jmu.edu>
2021-10-26 20:02:28 -0400

    cpy pst

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const service = express();
-
 const port = 5000;
+
+const fs = require("fs");
+const mysql = require("mysql");
+
+const json = fs.readFileSync("credentials.json", "utf8");
+const credentials = JSON.parse(json);
+
+const connection = mysql.createConnection(credentials);
+connection.connect((error) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
+
 service.listen(port, () => {
   console.log(`We're live on port ${port}!`);
 });
-
-/*  
-    The HTTP specification defines several different kinds of requests—or methods—that 
-    clients can send to services. These methods include GET, POST, DELETE, and PATCH. 
-    These methods roughly correspond to the four basic operations you use to interact 
-    with records in a database: create, read, update, or delete. This collection of 
-    operations is sometimes abbreviated as CRUD.
-*/
