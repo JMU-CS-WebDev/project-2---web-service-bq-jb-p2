d23366c6ca49f50d11c935119777de8b6241a1ea
Brandon P. Quinn <quinnbp@dukes.jmu.edu>
2021-10-27 13:22:48 -0400

    fuck

---
+++ create-db.sql
@@ -0,0 +1,22 @@
+-- Remove any existing database and user.
+DROP DATABASE IF EXISTS recomendations;
+DROP USER IF EXISTS rec_user@localhost;
+
+CREATE DATABASE recomendations CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE USER rec_user@localhost IDENTIFIED WITH mysql_native_password BY 'sunsh1n3';
+GRANT ALL PRIVILEGES ON recomendations.* TO rec_user@localhost;
+
+
+DROP TABLE IF EXISTS productions;
+
+CREATE TABLE productions (
+  p_id SERIAL PRIMARY KEY,
+  p_name VARCHAR(64),
+  p_type VARCHAR(64),
+  p_genre VARCHAR(64),
+  score INT,
+  summary VARCHAR(64)
+);
+
+INSERT INTO productions (p_id, p_name, p_type, p_genre, score, summary)
+VALUES (1, 'Breaking Bad', 'Show', 'Drama', 10, 'A highscool chemistry teacher is diagnosed...');
